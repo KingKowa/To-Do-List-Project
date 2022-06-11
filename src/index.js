57e0d674c2bf2eba00fafd00fa9fc23c8a816bce
KingKowa <ankomahfrederickyahoo@gmail.com>
2022-06-11 06:37:30 +0000

    Fix linter errors with index.js file

--- src/index.js
+++ src/index.js
@@ -11,6 +11,38 @@ class TodoObject {
   }
 }
 
+const removeTodoList = (todo) => {
+  todoListSection.removeChild(todo);
+  let count = 0;
+  const localdata = JSON.parse(localStorage.getItem('todolist'));
+  const data = Array.from(localdata).filter((i) => i.completed === false);
+  data.map((i) => i.index = count++);
+  localStorage.setItem('todolist', JSON.stringify(data));
+};
+
+const editTodoList = (todoContainer, todoDescription) => {
+  const editInputText = document.createElement('input');
+  editInputText.type = 'text';
+  editInputText.classList = 'editInput';
+  editInputText.value = todoDescription.textContent;
+  todoContainer.replaceChild(editInputText, todoDescription);
+  editInputText.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+      const todoContainers = document.querySelectorAll('.todoContainer');
+      const storageList = JSON.parse(localStorage.getItem('todolist'));
+      for (let i = 0; i < todoContainers.length; i += 1) {
+        if (todoContainers[i].classList.contains('clickedcheckbox')) {
+          storageList[i].description = editInputText.value;
+          localStorage.setItem('todolist', JSON.stringify(storageList));
+        }
+      }
+      editInputText.parentElement.classList.remove('clickedcheckbox');
+      todoContainer.replaceChild(todoDescription, editInputText);
+      todoDescription.textContent = editInputText.value;
+    }
+  });
+};
+
 const todoListArr = [];
 
 const addTodo = (todoDescription) => {
@@ -52,39 +84,10 @@ const addTodo = (todoDescription) => {
       i.parentElement.classList.toggle('clickedcheckbox');
       removeTodoList(i.parentElement);
     });
-  })
-};
-
-const editTodoList = (todoContainer,todoDescription) => {
-  const editInputText = document.createElement('input');
-  editInputText.type = 'text';
-  editInputText.classList = 'editInput';
-  editInputText.value = todoDescription.textContent;
-  todoContainer.replaceChild(editInputText, todoDescription);
-  editInputText.addEventListener('keypress', (e) => {
-    if (e.key === 'Enter'){
-      const todoContainers = document.querySelectorAll('.todoContainer');
-      const storageList = JSON.parse(localStorage.getItem('todolist'));
-      for (let i = 0; i < todoContainers.length; i += 1){
-        if (todoContainers[i].classList.contains('clickedcheckbox')){
-          storageList[i].description = editInputText.value;
-          localStorage.setItem('todolist', JSON.stringify(storageList));
-        }
-      }
-      editInputText.parentElement.classList.remove('clickedcheckbox');
-      todoContainer.replaceChild(todoDescription, editInputText);
-      todoDescription.textContent = editInputText.value;
-    }
   });
 };
 
-const removeTodoList = (todo) => {
-  todoListSection.removeChild(todo);
-  let count = 0;
-  const localdata = JSON.parse(localStorage.getItem('todolist'));
-  const data = Array.from(localdata).filter((i) => i.completed === false);
-  data.map((i) => i.index = count++);
-};
+
 
 todoText.addEventListener('keypress', (event) => {
   if (event.key === 'Enter' && todoText.value) {
